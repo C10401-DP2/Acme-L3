cde46e51cbbfc6fb3e2c22c759ac559ee4cac0d2
David del Hoyo Carcaboso <davdelcar@alum.us.es>
2023-02-20 18:40:17 +0100

    Closed #38 Crear clase bulletin

---
+++ src/main/java/acme/entities/bulletin/Bulletin.java
@@ -0,0 +1,55 @@
+
+package acme.entities.bulletin;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Bulletin extends AbstractEntity {
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date				moment;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			nick;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			message;
+
+	protected Boolean			isCritical;
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
