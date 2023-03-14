5d733ddffb8784f59d2a6c46cd1c931804070672
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-03-14 11:37:52 +0100

    Entidad Course

---
+++ src/main/java/acme/entities/course/Course.java
@@ -0,0 +1,44 @@
+
+package acme.entities.course;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.datatypes.ActivityType;
+import acme.framework.components.datatypes.Money;
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Course extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3} [0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			aBstract;
+
+	protected ActivityType		activityType;
+
+	protected Money				retailPrice;
+
+	@URL
+	protected String			link;
+}
