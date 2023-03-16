f16d4a9ff32215d348bed7a9cf9372539eba39b9
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-03-16 18:28:58 +0100

    Añadido import course

--- src/main/java/acme/entities/enrolment/Enrolment.java
+++ src/main/java/acme/entities/enrolment/Enrolment.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.entities.course.Course;
 import acme.framework.data.AbstractEntity;
 import acme.roles.Student;
 import lombok.Getter;
