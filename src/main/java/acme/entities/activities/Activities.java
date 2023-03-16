2ca4dc552ed0c7fc1be86e8cb7072cdcb01650bf
pablo_absqwdw <pablo_absqwdw@Pablo01>
2023-03-16 13:48:31 +0100

    Continuacion

--- src/main/java/acme/entities/activities/Activities.java
+++ src/main/java/acme/entities/activities/Activities.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.URL;
 
+import acme.datatypes.ActivityType;
 import acme.entities.enrolment.Enrolment;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
