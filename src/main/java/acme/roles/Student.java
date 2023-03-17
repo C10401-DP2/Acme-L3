23a1695164fce690d2200f332ed7fc7407f361e9
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-03-17 15:56:32 +0100

    Student csv

--- src/main/java/acme/roles/Student.java
+++ src/main/java/acme/roles/Student.java
@@ -2,15 +2,12 @@
 package acme.roles;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.course.Course;
 import acme.framework.data.AbstractRole;
 import lombok.Getter;
 import lombok.Setter;
@@ -40,11 +37,6 @@ public class Student extends AbstractRole {
 
 	@URL
 	protected String			link;
-
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	protected Course			courses;
 
 	@NotNull
 	protected Boolean			draftMode;
