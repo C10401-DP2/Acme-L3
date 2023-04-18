a2040f88920187effd9ddfae9276187ee1316b14
Adrián García Chavero <adrgarcha@alum.us.es>
2023-04-18 19:56:36 +0200

    Authenticated principals on audits finalizado.

---
+++ src/main/java/acme/features/authenticated/audit/AuthenticatedAuditListService.java
@@ -0,0 +1,52 @@
+
+package acme.features.authenticated.audit;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.audit.Audit;
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AuthenticatedAuditListService extends AbstractService<Authenticated, Audit> {
+
+	// Internal state
+	@Autowired
+	protected AuthenticatedAuditRepository repository;
+
+
+	// Interface
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Audit> objects;
+
+		objects = this.repository.findAllAudits();
+		super.getBuffer().setData(objects);
+	}
+
+	@Override
+	public void unbind(final Audit object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "code", "conclusion");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
---
+++ src/main/java/acme/features/authenticated/audit/AuthenticatedAuditRepository.java
@@ -0,0 +1,25 @@
+
+package acme.features.authenticated.audit;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.audit.Audit;
+import acme.framework.repositories.AbstractRepository;
+import acme.roles.Auditor;
+
+@Repository
+public interface AuthenticatedAuditRepository extends AbstractRepository {
+
+	@Query("SELECT a FROM Audit a")
+	Collection<Audit> findAllAudits();
+
+	@Query("SELECT a FROM Audit a WHERE a.id = :id")
+	Audit findAuditById(int id);
+
+	@Query("SELECT a.auditor FROM Audit a WHERE a.id = :id")
+	Auditor findAuditorByAuditId(int id);
+
+}
