32abc96acd98c4452ee2b6f1011963e38f143fce
David <David@DESKTOP-63TSB31.home>
2023-05-01 13:33:10 +0200

    clean

--- src/main/java/acme/entities/practicumSession/PracticumSession.java
+++ src/main/java/acme/entities/practicumSession/PracticumSession.java
@@ -10,6 +10,7 @@ import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
